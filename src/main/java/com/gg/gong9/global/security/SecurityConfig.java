4ac0afeacb67087db8cf8f641275788dfe8af33b
wjdwltn <jlsowltn2000@naver.com>
2025-09-05 13:01:59 +0900

    fix : security 루트 경로 추가

--- src/main/java/com/gg/gong9/global/security/SecurityConfig.java
+++ src/main/java/com/gg/gong9/global/security/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/users/**",
                                 "/auth/**",
                                 "/sms/**",
